37e696b63ca160219963e5407adce58d5b4d7f20
sarveshwar-s <59520591+sarveshwar-s@users.noreply.github.com>
2021-12-16 19:03:31 +0100

    Added db queries

--- pipelines/inference.py
+++ pipelines/inference.py
@@ -6,6 +6,9 @@ from train import preprocessing_pipeline, prepare_data
 from joblib import load
 import mlflow
 from datetime import datetime
+import psycopg2
+from config import config
+
 
 def load_model(path: str) -> object:
   loaded_model = load(path)
@@ -25,7 +28,9 @@ def inference(test_path: str, model_path: str) -> np.ndarray:
     data_master = prepare_data(test_path)
     data = data_master.copy()
     # data = data.drop(labels=["day"], axis=1) # dropping the day column.
-    data_to_db = data
+    # This is the features to be sent to the database
+    data_to_db = data_master.copy()
+
     data = data.drop(labels=["day"], axis=1) #dropping the day column
     processed_df = preprocessing_pipeline(data, isInference=True)
 
@@ -36,9 +41,26 @@ def inference(test_path: str, model_path: str) -> np.ndarray:
 
     predicted_price = model.predict(processed_df)
 
-    
+    if len(predicted_price) != 0:
+      insert_update_to_db(data_to_db, predicted_price)
     
     return predicted_price
+
+def insert_update_to_db(data_to_db, predicted_price):
+  data_to_db["energy_sum"] = predicted_price
+  final_processed_data_to_db = data_to_db.to_numpy()
+  query = "INSERT INTO public.predicted_data( appliance_id, day, temperaturemax, temperaturemin, pred_energy_cons) VALUES (%s, %s, %s,%s, %s)"
+  try:
+    params = config()
+    conn = psycopg2.connect(**params)
+    cur = conn.cursor()
+    cur.executemany(query, final_processed_data_to_db)
+    conn.commit()
+    cur.close()
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+  pass
 
 def prepare_data(filepath):
     energy_data = pd.read_csv(filepath)
